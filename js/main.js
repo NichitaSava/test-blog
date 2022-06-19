427250d6498e46d8df92eb55cf84875e274d1828
nik_sava <nichitarich@gmail.com>
2022-06-19 15:47:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+
+    $('.slider_inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinte: false
+    });
+    $('select').styler();
+    
+    $('.header_btn-menu').on('click', function(){
+        $('.menu ul').slideToggle();
+    });
+
+});
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
